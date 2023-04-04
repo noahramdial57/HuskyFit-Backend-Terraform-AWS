b6c821efb106e2bc4f68c345a3cda2caaf76e4b9
noahramdial57 <noahramdial57@gmail.com>
2023-04-04 13:52:31 -0400

    Got a working configuration

--- main.tf
+++ main.tf
@@ -1,14 +1,32 @@
 # Configure the AWS Provider
 # access key and secret access key were set in env vars
 
-provider "aws" {
-#   version = "~> 2.0"  # this is deprecated
-  region  = "us-east-1"
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.5.0"
+
+    }
+  }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
 
-resource "aws_s3_bucket" "huskyfit-bucket-test-tf" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "my-tf-test-bucket" {
+  bucket = "huskyfit-bucket-test-tf" # name has to be unique
 
+}
 
+# Monitor spendings
+resource "aws_budgets_budget" "monthly-budge" {
+  name              = "HuskyFit-Budget"
+  budget_type       = "COST"
+  limit_amount      = "10"
+  limit_unit        = "USD"
+  time_period_start = "2023-04-01_00:00"
+  time_unit         = "MONTHLY"
 }
